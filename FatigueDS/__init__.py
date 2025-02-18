c106302799a52400c7657badab4915f96de0190d
jasasonc <jasasonc@gmail.com>
2025-02-18 12:30:40 +0100

    pyproject file and version fix

--- FatigueDS/__init__.py
+++ FatigueDS/__init__.py
@@ -2,7 +2,7 @@
 A project template for the sdPy effort..
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 from .spec_dev import SpecificationDevelopment
 from . import tools
 from . import signals
